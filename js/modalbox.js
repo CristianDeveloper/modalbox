ff7f15e1cab22005d1d5eaba01db6a8697c98a4c
CristianDeveloper <dev.cristian99@gmail.com>
2016-08-10 16:33:46 +0300

    Create modalbox.js

---
+++ js/modalbox.js
@@ -0,0 +1,40 @@
+/**
+ * Modal box JavaScript
+ * @Version 1.0
+ * @Author: Cristian Marian
+ * @E-mail: dev.cristian99@gmail.com
+ */
+
+//Get Open Button
+var open = document.getElementById('open-modal');
+
+//Get Close Button 
+var close = document.getElementById('close-modal');
+
+//Get Modal Contend
+var content = document.getElementById('modal-content');
+
+//Get Modal Target (Ex: data-target="modal-name")
+var element = document.getElementById('open-modal').getAttribute('data-target');
+
+//Set Modal Name
+var modal = document.getElementById(element);
+
+//Open Modal
+open.onclick = function() {
+    modal.style.display = "block";
+    content.style.animation = "open-modal-animation";
+    content.style.animationDuration = "0.5s";
+}
+
+//Close Modal
+close.onclick = function() {
+    modal.style.display = "none";
+} 
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
